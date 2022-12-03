a419101b2927313e686d5bb7346a8be7a3ca256b
Jirlan Souza <jirlansouza08@gmail.com>
2022-12-03 15:02:52 -0400

    feat: cretaed the getTotal methods

--- src/Order.ts
+++ src/Order.ts
@@ -1,14 +1,31 @@
+import { Coupon } from "./Coupon";
 import { Cpf } from "./Cpf";
+import { Item } from "./Item";
 import { OrderItem } from "./OrderItem";
 
 export class Order {
     cpf: Cpf;
     orderItens: OrderItem[] = [];
+    coupon?: Coupon;
+
     constructor(cpf: string) {
         this.cpf = new Cpf(cpf);
     }
 
-    addItem(item: OrderItem) {
-        this.orderItens.push(item);
+    addItem(item: Item, quantity: number) {
+        this.orderItens.push(new OrderItem(item.idItem, item.price, quantity));
+    }
+
+    getTotal() {
+        let total = this.orderItens.reduce(
+            (total, orderItem) => total + orderItem.getTotal(),
+            0,
+        );
+
+        if (this.coupon) {
+            total -= this.coupon.calculateDiscount(total);
+        }
+
+        return total;
     }
 }
--- src/OrderItem.ts
+++ src/OrderItem.ts
@@ -2,8 +2,12 @@ import { Item } from "./Item";
 
 export class OrderItem {
     constructor(
-        readonly idItem: Item,
+        readonly idItem: string,
         readonly price: number,
         readonly quantity: number,
     ) {}
+
+    getTotal() {
+        return this.price * this.quantity;
+    }
 }
